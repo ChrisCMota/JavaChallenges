9de01b5c4fb9ad2a9cb3fb4db6f6f071f5d88ca5
ChrisCMota <christiancmota@outlook.com>
2024-04-16 16:09:49 +0100

    Update WhileLoopChall.java
    Part 2 of while loop challenge

--- practice/WhileLoopChall.java
+++ practice/WhileLoopChall.java
@@ -2,6 +2,8 @@ package jpnn.JavaChallenges.practice;
 
 public class WhileLoopChall {
     
+    int evenNummberCounter = 0;
+    int oddNumberCounter = 0;
 
     public boolean isEvenNumber(int number){
         if (number % 2 == 0) {
@@ -14,13 +16,20 @@ public class WhileLoopChall {
         int counter = 5;
         while (counter < 21) {
             if (isEvenNumber(counter)) {
+                evenNummberCounter++;
                 System.out.println(counter);
                 counter++;
+                if (evenNummberCounter == 5) {
+                    break;
+                }
             }else{
                 counter++;
+                oddNumberCounter++;
                 continue;
             }
         }
+        System.out.println("Even Numbers = " + evenNummberCounter);
+        System.out.println("Odd Numbers = " + oddNumberCounter);
     }
 
 }
