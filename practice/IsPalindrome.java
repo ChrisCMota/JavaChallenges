8fff813d334cf8164772441da2429825383451eb
ChrisCMota <christiancmota@outlook.com>
2024-04-18 01:06:26 +0100

    Palindrome chall
    reversing a number and comparing it

---
+++ practice/IsPalindrome.java
@@ -0,0 +1,19 @@
+package jpnn.JavaChallenges.practice;
+
+public class IsPalindrome {
+    public static boolean isPalindrome(int number){
+        //Store the reversed of the input number
+        int reverse = 0;
+        //Store the original value because the input number will be modified during the process of reversing it
+        int original = number;
+
+        while (original != 0) {
+            //reverse the original number by taking the last digit and appending it to the reverse
+            //after shifting one place to the left multiplying by 10
+            reverse = reverse * 10 + original % 10;
+            //remove the last digit by integer divison, shifting to the right
+            original /= 10;
+        }
+        return reverse == number;
+    }
+}
--- test/TestMain.java
+++ test/TestMain.java
@@ -1,11 +1,9 @@
 package jpnn.JavaChallenges.test;
 
-import jpnn.JavaChallenges.practice.WhileLoopChall;
+
 
 public class TestMain {
     public static void main(String[] args) {
-        WhileLoopChall counter = new WhileLoopChall();
-
-        counter.printEvenNumbersFrom5To20();
+         
     }
 }
