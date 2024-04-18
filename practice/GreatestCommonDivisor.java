8b5ee86b1b5af122bd1b31ff917d8eb6da01c2e1
ChrisCMota <christiancmota@outlook.com>
2024-04-18 16:36:30 +0100

    Create GreatestCommonDivisor.java
    Greatest common divisor challenge

---
+++ practice/GreatestCommonDivisor.java
@@ -0,0 +1,27 @@
+package jpnn.JavaChallenges.practice;
+
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10)
+            return -1;
+
+        int commonDivisor = 0;
+
+        if (first > second) {
+            for (int i = 1; i <= first; i++) {
+                if ((first % i == 0) && (second % i == 0)) {
+                    commonDivisor = i;
+                }
+            }
+        } else {
+            for (int j = 1; j <= second; j++) {
+                if ((second % j == 0) && (first % j == 0)) {
+                    commonDivisor = j;
+                }
+            }
+        }
+        return commonDivisor;
+
+    }
+}
